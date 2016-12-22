e8412b89cb5fb59a55db0866d1e8b0b16af1ddf7
SjaakNijland <wafelcool@gmail.com>
2016-12-22 09:42:16 +0100

    edam-volendam

---
+++ head.php
@@ -0,0 +1,7 @@
+
+        <head>
+            <title>Home</title>
+            <link rel="stylesheet" href="style/style.css" type="text/css">
+            <meta charset="utf-8">
+        </head>
+        <body>
